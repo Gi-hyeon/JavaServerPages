c566b0edc207ceab80afc91cfe200fb45049e436
GuGihyeon <ggh4177@gmail.com>
2023-04-28 17:20:57 +0900

    Create MemberDAO.java

---
+++ JSPBook/src/totaltest/dao/MemberDAO.java
@@ -0,0 +1,23 @@
+package totaltest.dao;
+
+import java.util.ArrayList;
+
+import totaltest.vo.MemberVO;
+
+public class MemberDAO{
+	
+	private ArrayList<MemberVO> memberList = new ArrayList<MemberVO>();
+	private static MemberDAO instance = new MemberDAO();
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	
+	public ArrayList<MemberVO> getMemberList() {
+		return memberList;
+	}
+
+	public void insertMember(MemberVO member) {
+		memberList.add(member);
+	}
+
+}
