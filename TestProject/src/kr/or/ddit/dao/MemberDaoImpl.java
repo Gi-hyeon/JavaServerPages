acd5dbadff1c7e38c6f6d6c675fc1f199c34a9bd
GuGihyeon <ggh4177@gmail.com>
2023-05-02 17:24:25 +0900

    Update MemberDaoImpl.java

--- TestProject/src/kr/or/ddit/dao/MemberDaoImpl.java
+++ TestProject/src/kr/or/ddit/dao/MemberDaoImpl.java
@@ -1,5 +1,9 @@
 package kr.or.ddit.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import kr.or.ddit.util.MybatisSqlSessionFactory;
@@ -33,6 +37,47 @@ public class MemberDaoImpl implements IMemberDao {
 				session.close();
 			}
 		}
+		return cnt;
+	}
+	
+	@Override
+	public MemberVO selectMemberByIdPw(String mem_id, String mem_pw) {
+		// TODO Auto-generated method stub
+		MemberVO vo = null;
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+	        Map<String, String> params = new HashMap<>();
+	        params.put("mem_id", mem_id);
+	        params.put("mem_pw", mem_pw);
+	        vo = session.selectOne("board.selectMemberByIdPw", params);
+		} finally {
+			if (session != null) {
+				session.commit();
+				session.close();
+			}
+		}
+		
+		return vo;
+	}
+
+	@Override
+	public int idDuplication(String mem_id) {
+		// TODO Auto-generated method stub
+		int cnt = 0;
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			cnt = session.selectOne("board.idDuplication", mem_id);
+		} finally {
+			if (session != null) {
+				session.commit();
+				session.close();
+			}
+		}
+		
 		return cnt;
 	}
 }
