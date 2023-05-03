953628a31f782d05bb83b6ec6b01f0cb4bfd95cd
GuGihyeon <ggh4177@gmail.com>
2023-05-03 16:26:30 +0900

    Update IBoardService.java

--- TestProject/src/kr/or/ddit/service/IBoardService.java
+++ TestProject/src/kr/or/ddit/service/IBoardService.java
@@ -1,5 +1,10 @@
 package kr.or.ddit.service;
 
+import java.util.List;
+
+import kr.or.ddit.vo.BoardVO;
 
 public interface IBoardService {
+	// 전체 게시글 조회 메소드
+	public List<BoardVO> selectAllBoard();
 }
