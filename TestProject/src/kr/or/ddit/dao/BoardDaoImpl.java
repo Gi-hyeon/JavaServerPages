dd7e8e60da87812404a144fd59ba428cd87b91bc
GuGihyeon <ggh4177@gmail.com>
2023-05-03 16:26:26 +0900

    Update BoardDaoImpl.java

--- TestProject/src/kr/or/ddit/dao/BoardDaoImpl.java
+++ TestProject/src/kr/or/ddit/dao/BoardDaoImpl.java
@@ -1,5 +1,12 @@
 package kr.or.ddit.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.ddit.util.MybatisSqlSessionFactory;
+import kr.or.ddit.vo.BoardVO;
+
 public class BoardDaoImpl implements IBoardDao {
 	private static BoardDaoImpl dao;
 	
@@ -11,5 +18,24 @@ public class BoardDaoImpl implements IBoardDao {
 		}
 		
 		return dao;
+	}
+
+	@Override
+	public List<BoardVO> selectAllBoard() {
+		// TODO Auto-generated method stub
+		List<BoardVO> list = null;
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			list = session.selectList("board.selectAllBoard");
+		} finally {
+			if (session != null) {
+				session.commit();
+				session.close();
+			}
+		}
+		
+		return list;
 	}
 }
